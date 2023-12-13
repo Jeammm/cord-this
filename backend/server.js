1db9cfe2f48d4711a37086a25f2d0ff78aa0bda3
Jeammm <87964996+Jeammm@users.noreply.github.com>
2023-12-13 21:29:50 +0700

    fix cors 2

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ["http://localhost:3000", "https://cordthis.onrender.com"],
+    origin: ["http://localhost:3000", "https://localhost:3000", "https://cordthis.onrender.com"],
   },
 });
 
